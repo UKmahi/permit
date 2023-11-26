47b775a2781f778c30f83c2c40dfe6539bde9baa
UKmahi <utkarshkrmahi@gmail.com>
2023-11-26 20:28:14 +0530

    Files updated

---
+++ practice.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+int main()
+{
+    long long int A,B,C,D;
+    cin>>A>>B>>C>>D;
+    
+    if(pow(A,B)>pow(C,D)){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+    
+} 
